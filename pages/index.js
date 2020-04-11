e9459457e8df1a3d33bb9db049c9889c37ec0f41
Marcos Zillig <marcos.zillig@gmail.com>
2020-04-11 09:15:50 -0300

    Commit inicial

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import Head from 'next/head'
 
-const Home = () => (
+const Home = (props) => (
   <div className="container">
     <Head>
       <title>Create Next App</title>
@@ -8,43 +8,13 @@ const Home = () => (
     </Head>
 
     <main>
-      <h1 className="title">
-        Welcome to <a href="https://nextjs.org">Next.js!</a>
-      </h1>
-
-      <p className="description">
-        Get started by editing <code>pages/index.js</code>
-      </p>
-
-      <div className="grid">
-        <a href="https://nextjs.org/docs" className="card">
-          <h3>Documentation &rarr;</h3>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a href="https://nextjs.org/learn" className="card">
-          <h3>Learn &rarr;</h3>
-          <p>Learn about Next.js in an interactive course with quizzes!</p>
-        </a>
-
-        <a
-          href="https://github.com/zeit/next.js/tree/master/examples"
-          className="card"
-        >
-          <h3>Examples &rarr;</h3>
-          <p>Discover and deploy boilerplate example Next.js projects.</p>
-        </a>
-
-        <a
-          href="https://zeit.co/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          className="card"
-        >
-          <h3>Deploy &rarr;</h3>
-          <p>
-            Instantly deploy your Next.js site to a public URL with ZEIT Now.
-          </p>
-        </a>
-      </div>
+    <ul>
+      {props.movies.map(movie => (
+        <li key={movie.id}>
+          {movie.title}
+        </li>
+      ))}
+    </ul>
     </main>
 
     <footer>
@@ -199,5 +169,14 @@ const Home = () => (
     `}</style>
   </div>
 )
+Home.getInitialProps = async function() {
+  const res = await fetch('https://api.themoviedb.org/3/movie/popular?api_key=4cdf98dae1ad5192544b610b08d033c5');
+  const data = await res.json();
 
+  console.log(data.results);
+
+  return {
+    movies: data.results.map(entry => entry)
+  };
+};
 export default Home
